ac2e5a9d0d8775a84a00a5c47379145b766e7715
armanjotkaur <armanjotkaur@academic.com>
2023-10-22 20:15:59 -0500

    sdf assignment 2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Module_02 Assignment02 Resubmission
 ## Description
+- SIMPLE DATA TYPES.  In simple data types i mentioned the city in which I was born and printed the number of days in a week.
+- CALCULATIONS. The calculation of Refridgerator in GST and PST.
+- COLLECTIONS.  The collection contains lists, tuples, dictionaries, and sets . In list i entered my first name and print it, In tuples i mentioned all my current courses that i am learning ,  In dictionaries i printed nations with its values and also index canada and at the last in sets i done union, intersection,and difference print of first and second set.
--- sdf_assignment_2.py
+++ sdf_assignment_2.py
@@ -6,10 +6,78 @@ Usage:To calculate data type.
 """
 
 # SIMPLE DATA TYPES
+# Declare a variable to store a city
+city = "Sri Muktsar Sahib"
+print(f'value: {city} type: {type(city)}')
+
+
+
+weekdays = "7"
+print(f'value: {weekdays} type: {type(weekdays)}')
+
+
+
+
 
 # CALCULATIONS
 
+GST = 0.05
+PST = 0.07
+CP_Refridgerator = 2995.95
+GST_ON_REFRIDGERATOR = CP_Refridgerator  * GST
+PST_ON_REFRIDGERATOR = CP_Refridgerator * PST
+PRICE = CP_Refridgerator + GST + PST
+print(f"Refridgerator: ${CP_Refridgerator:.2f} , GST: ${GST_ON_REFRIDGERATOR:.2f} ,  PST: ${PST_ON_REFRIDGERATOR:.2f} ,  TOTAL: ${PRICE:.2f}")
+ 
+
+
+
+
+
+
 # COLLECTIONS
+
+lists = [1,5,22,7,23,86,12]
+
+print(lists)
+
+my_firstname = "Arman"
+lists.insert(5, my_firstname)
+print(lists)
+
+lists.remove(7)
+print(lists)
+
+# TUPLES
+courses = ("SDF" , "MATH" , "SERVICE MANAGEMENT" , "PROFESSIONAL DEVELOPMENT" , "INFORMATION SYSTEMS" , "COMMUNICATION STRATEGIES")
+print(courses)
+
+# DICTIONARIES 
+nations = {"CANADA" : 38781291 , "USA" : 339986563 , "MEXICO" : 128465567}
+print(nations)
+# INDEX
+nations["CANADA"] = 40000000
+
+nations["BRAZIL"] = 216422446
+print(nations)
+
+# SETS
+first_set = {1,17,54,21,12,77,23,12}
+print(first_set)
+second_set = {2,5,17,18,19,77,78,12}
+print(second_set)
+unique_set = first_set.union(second_set)
+print(unique_set)
+set = second_set.intersection(first_set)
+print(set)
+other_set = second_set.difference(first_set)
+print(other_set)
+
+
+
+
+
+
 
 
 
